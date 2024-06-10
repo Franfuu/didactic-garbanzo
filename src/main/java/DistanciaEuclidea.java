f3ac70e84a0c78be6b7ff243065549d790a09029
Sangarru11 <santiagogarciaruiz665@gmail.com>
2024-06-10 09:13:20 +0200

    Comentarios

--- src/main/java/DistanciaEuclidea.java
+++ src/main/java/DistanciaEuclidea.java
@@ -1,19 +1,34 @@
+/**
+ * La clase DistanciaEuclidea calcula la distancia entre origen y destino
+ * @author Santiago Garcia Ruiz
+ */
 class DistanciaEuclidea extends Distancia {
     public DistanciaEuclidea() {
     }
+    /**
+     * El metodo DistanciaEuclidea
+     * @param origen_x Punto x de origen
+     * @param origen_y Punto y de origen
+     * @param destino_x Punto x de destino
+     * @param destino_y Punto y de destino
+     */
 
-    // Constructor
     public DistanciaEuclidea(double origen_x, double origen_y, double destino_x, double destino_y) {
         super(origen_x, origen_y, destino_x, destino_y);
     }
 
-    // Implementación del método para calcular la distancia euclidiana
-
+    /**
+     * El metodo calcularDistancia
+     * @param origen_x Punto x de origen
+     * @param origen_y Punto y de origen
+     * @param destino_x Punto x de destino
+     * @param destino_y Punto y de destino
+     * @return La distancia euclidiana entre el punto de origen y el punto de destino.
+     */
     double calcularDistancia(double origen_x, double origen_y, double destino_x, double destino_y) {
         return Math.sqrt(Math.pow((destino_x - origen_x), 2) + Math.pow((destino_y - origen_y), 2));
     }
-
-    // Método principal para probar la clase
+    
     public static void main(String[] args) {
         double origen_x = 0;
         double origen_y = 0;
@@ -25,6 +40,12 @@ class DistanciaEuclidea extends Distancia {
         System.out.println("La distancia euclidiana entre los puntos es: " + distancia);
     }
 
+    /**
+     *  El metodo medir mide entre origen y destino
+     * @param origen Punto origen
+     * @param destino Punto destino
+     * @returnm El punto de origen y el punto de destino
+     */
     @Override
     public double medir(Punto origen, Punto destino) {
         return 0;
